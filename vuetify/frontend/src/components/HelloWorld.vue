36d966078a5f489d3ecb66581520f93ac2b9d87e
Raphael Martin <raphael.martin@newm.com.br>
2019-06-17 10:55:51 -0300

    Primeiro commit

---
+++ vuetify/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex class="box" sm4>
+        <p class="text-xs-center">teste teste teste teste</p>
+      </v-flex>
+      <v-flex class="box" sm6 :xs11="xs">
+        <p class="text-xs-center">teste</p>
+      </v-flex>
+      <v-flex class="box" sm5>
+        <p class="text-xs-center">teste</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      xs: true
+    })
+  }
+</script>
+
+<style>
+  .box {
+    border: 1px solid
+  }
+</style>
